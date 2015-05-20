0cf4d63a18ed603eb1658ddcb99598689502cb0b
Michaelangelo Jong <mjong@compilr.com>
2015-05-20 16:27:41 -0300

    npm init

---
+++ index.js
@@ -0,0 +1,13 @@
+var Generator = require('generate-js');
+
+var JSDocs = Generator.generate(function JSDocs() {
+    var _ = this;
+
+    _.defineProperties({
+    });
+});
+
+JSDocs.definePrototype({
+});
+
+module.exports = JSDocs;
